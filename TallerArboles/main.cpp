ed2fcd405db9784a9ace2f89e3b0dfc3a22cb77b
janmunoz21 <janmarconep@hotmail.com>
2024-09-18 11:45:30 -0500

    Create main.cpp

---
+++ TallerArboles/main.cpp
@@ -0,0 +1,6 @@
+/****************************************
+Autor: Jan Marco Muñoz Pineda
+Fecha: 28 de agosto 2024
+Materia: Estructura de Datos
+Tema: Laboratorio Implementación Arboles
+****************************************/
